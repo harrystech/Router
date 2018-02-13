04b10002f44c0b8996ea6f361d8e7aa4cbce2b26
Aaron Rosen <azrosen92@gmail.com>
2018-02-13 14:12:37 -0500

    [Router] Updates for Swift 4.

--- Source/Router.swift
+++ Source/Router.swift
@@ -19,14 +19,14 @@ import UIKit
 public typealias RouteHandler = (_ req: Request) -> Void
 
 open class Router {
-    
+
     fileprivate var orderedRoutes = [Route]()
     fileprivate var routes = [Route: RouteHandler]()
-    
+
     public init() {}
     /**
         Binds a route to a router
-    
+
         - parameter aRoute: A string reprsentation of the route. It can include url params, for example id in /video/:id
         - parameter callback: Triggered when a route is matched
     */
@@ -41,67 +41,67 @@ open class Router {
             fatalError("[\(aRoute)] unknown bind error")
         }
     }
-    
+
     /**
         Matches an incoming URL to a route present in the router. Returns nil if none are matched.
-    
+
         - parameter url: An URL of an incoming request to the router
         - returns: The matched route or nil
     */
     open func match(_ url: URL) -> Route? {
-        
+
         guard let routeComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
             return nil
         }
-        
+
         // form the host/path url
         let host = routeComponents.host.flatMap({"/\($0)"}) ?? ""
-        let path = routeComponents.path 
+        let path = routeComponents.path
         let routeToMatch = "\(host)\(path)"
         let queryParams = routeComponents.queryItems
         var urlParams = [URLQueryItem]()
-        
+
         // match the route!
         for route in orderedRoutes {
             guard let pattern = route.routePattern else {
                 continue
             }
-              
+
             var regex: NSRegularExpression
-              
+
             do {
                 regex = try NSRegularExpression(pattern: pattern,
                 options: .caseInsensitive)
             } catch let error as NSError {
                 fatalError(error.localizedDescription)
             }
-              
+
             let matches = regex.matches(in: routeToMatch, options: [],
                 range: NSMakeRange(0, routeToMatch.characters.count))
-                    
+
             // check if routeToMatch has matched
             if matches.count > 0 {
                 let match = matches[0]
-                        
+
                 // gather url params
                 for i in 1 ..< match.numberOfRanges {
                     let name = route.urlParamKeys[i-1]
-                    let value = (routeToMatch as NSString).substring(with: match.rangeAt(i))
+                    let value = (routeToMatch as NSString).substring(with: match.range(at: i))
                     urlParams.append(URLQueryItem(name: name, value: value))
                 }
-                        
+
                 // fire callback
                 if let callback = routes[route] {
                     callback(Request(aRoute: route, urlParams: urlParams, queryParams: queryParams))
                 }
-                        
+
                 // return route that was matched
                 return route
             }
         }
-        
+
         // nothing matched
         return nil
     }
-    
+
 }
